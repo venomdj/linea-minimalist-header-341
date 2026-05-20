d702ae9cd3d13b798a7d70203edb927c95141879
venomdj <99116393+venomdj@users.noreply.github.com>
2026-05-20 15:32:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/linea-minimalist-header-341/',
+    base: '/',
   
   server: {
     host: "::",
