58493ba53a41a0e146e323dd0a2ef2f1bfe0d203
venomdj <99116393+venomdj@users.noreply.github.com>
2026-05-20 15:24:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <CartProvider>
         <ScrollToTop />
         <Routes>
